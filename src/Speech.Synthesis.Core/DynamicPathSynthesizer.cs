ee9db0c8ae9e24de7833247fbcade9ab598878df
Xianyi Cui <cxy_910607@hotmail.com>
2019-03-02 22:49:50 +0800

    1. add dynamic path synthesizer 2. add custom synthesizer proto

---
+++ src/Speech.Synthesis.Core/DynamicPathSynthesizer.cs
@@ -0,0 +1,50 @@
+﻿namespace Speech.Synthesis.Core
+{
+    using Params;
+    using System;
+    using System.IO;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public abstract class DynamicPathSynthesizer<TParam, TEncode> : ISpeechSynthesizer<TParam, TEncode> where TParam : SynthesisParams
+    {
+        private readonly ISsmlConverter<TParam> _ssmlConverter;
+
+        protected DynamicPathSynthesizer() : this(SimpleSsmlConverter.Instance)
+        {
+        }
+
+        protected DynamicPathSynthesizer(ISsmlConverter<TParam> ssmlConverter)
+        {
+            _ssmlConverter = ssmlConverter;
+        }
+
+        public Stream GetSynthesizedData(string text, TParam synthesizeParams, TEncode outputEncode)
+        {
+            return Task.Run(async () =>
+                await GetSynthesizedDataAsync(text, synthesizeParams, outputEncode, CancellationToken.None)).Result;
+        }
+
+        public Stream GetSynthesizedData(string ssml, TEncode outputEncode)
+        {
+            return Task.Run(async () => await GetSynthesizedDataAsync(ssml, outputEncode, CancellationToken.None))
+                .Result;
+        }
+
+        public Task<Stream> GetSynthesizedDataAsync(string text, TParam synthesizeParams, TEncode outputEncode, CancellationToken token)
+        {
+            Uri host = null;
+            string ssml = _ssmlConverter.Convert(text, synthesizeParams);
+            return InternalSynthesisAsync(ssml, host, outputEncode, token);
+        }
+
+        public Task<Stream> GetSynthesizedDataAsync(string ssml, TEncode outputEncode, CancellationToken token)
+        {
+            Uri host = null;
+            return InternalSynthesisAsync(ssml, host, outputEncode, token);
+        }
+
+        protected abstract Task<Stream> InternalSynthesisAsync(string ssml, Uri targetEp, TEncode encode,
+            CancellationToken token);
+    }
+}
---
+++ src/Speech.Synthesis.Microsoft/CustomVoiceSynthesizer.cs
@@ -0,0 +1,54 @@
+﻿namespace Speech.Synthesis.Microsoft
+{
+    using Core;
+    using Core.Params;
+    using Extensions;
+    using System;
+    using System.IO;
+    using System.Net.Http;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class CustomVoiceSynthesizer : DynamicPathSynthesizer<SynthesisParams, AudioEncode>
+    {
+        private readonly HttpClient _synthesisClient;
+
+        public CustomVoiceSynthesizer() : this(SimpleSsmlConverter.Instance)
+        {
+        }
+
+        public CustomVoiceSynthesizer(ISsmlConverter<SynthesisParams> ssmlConverter) : base(ssmlConverter)
+        {
+            _synthesisClient = new HttpClient();
+        }
+
+        public CustomVoiceSynthesizer()
+        {
+            
+        }
+
+        protected override async Task<Stream> InternalSynthesisAsync(string ssml, Uri targetEp, AudioEncode encode, CancellationToken token)
+        {
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, targetEp)
+            {
+                Content = new StringContent(ssml, Encoding.UTF8, "application/ssml+xml")
+            };
+            request.Headers.Add("Connection", "Keep-Alive");
+            request.Headers.Add("X-Microsoft-OutputFormat", encode.GetEncodeName());
+            request.Headers.Add("X-FD-ClientID", "RadioStationService");
+            request.Headers.Add("X-FD-ImpressionGUID", Guid.NewGuid().ToString());
+
+            HttpResponseMessage response = await _synthesisClient.SendAsync(request, token).ConfigureAwait(false);
+            if (!response.IsSuccessStatusCode)
+            {
+                string errorMsg = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                throw new Exception(
+                    $"Synthesize fail with status code: {response.StatusCode.ToString()}, error message: [{errorMsg}]");
+            }
+            Stream stream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false);
+
+            return stream;
+        }
+    }
+}
