8cce603ba560576363de91068c079057337f052b
Xianyi Cui <xiacui@microsoft.com>
2019-04-26 22:05:45 +0800

    add ssml normalizer interface

---
+++ src/Speech.Synthesis.Core/ISsmlNormalizer.cs
@@ -0,0 +1,10 @@
+﻿namespace Speech.Synthesis.Core
+{
+    using System;
+
+    public interface ISsmlNormalizer<TArg>
+    {
+        event EventHandler<TArg> OnNormalized;
+        string Normalize(string ssmlText);
+    }
+}
