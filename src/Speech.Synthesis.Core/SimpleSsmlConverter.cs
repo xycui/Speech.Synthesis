e95ef18958cdf244fa40fc3fa3faa0ac30ab7766
Xianyi Cui <xiacui@microsoft.com>
2019-08-13 00:02:16 +0800

    fix language issue

--- src/Speech.Synthesis.Core/SimpleSsmlConverter.cs
+++ src/Speech.Synthesis.Core/SimpleSsmlConverter.cs
@@ -7,7 +7,7 @@
 
     public class SimpleSsmlConverter : ISsmlConverter<SynthesisParams>
     {
-        private const string SsmlTemplate = "<speak version=\"1.0\" xmlns=\"http://www.w3.org/2001/10/synthesis\" xmlns:mstts=\"http://www.w3.org/2001/mstts\" xml:lang=\"zh-CN\"><voice name=\"{0}\"><prosody rate=\"{1}\">{2}</prosody></voice></speak>";
+        private const string SsmlTemplate = "<speak version=\"1.0\" xmlns=\"http://www.w3.org/2001/10/synthesis\" xmlns:mstts=\"http://www.w3.org/2001/mstts\" xml:lang=\"{3}\"><voice name=\"{0}\"><prosody rate=\"{1}\">{2}</prosody></voice></speak>";
         private static readonly Lazy<ISsmlConverter<SynthesisParams>> LazyInstace = new Lazy<ISsmlConverter<SynthesisParams>>(() => new SimpleSsmlConverter());
         public static ISsmlConverter<SynthesisParams> Instance => LazyInstace.Value;
 
@@ -27,7 +27,7 @@
             string voiceFont = param.VoiceFont;
             string rateString = string.IsNullOrEmpty(param.RateString) ? "+00.00%" : param.RateString;
 
-            return string.Format(SsmlTemplate, voiceFont, rateString, validText);
+            return string.Format(SsmlTemplate, voiceFont, rateString, validText, param.Language);
         }
     }
 }
