69a309b100623898dcf08848e19e695e1741c643
Xianyi Cui <cxy_910607@hotmail.com>
2019-11-08 16:39:10 +0800

    update comments

--- src/Speech.Synthesis.Core/ISsmlNormalizer.cs
+++ src/Speech.Synthesis.Core/ISsmlNormalizer.cs
@@ -1,9 +1,17 @@
 ﻿namespace Speech.Synthesis.Core
 {
-    using System;
-
+    /// <summary>
+    /// It will normalize ssml and generate intermediate data
+    /// </summary>
+    /// <typeparam name="TInt">intermediate data type</typeparam>
     public interface ISsmlNormalizer<TInt>
     {
+        /// <summary>
+        /// Normalize method for the ssml text and generate the intermedia data.
+        /// </summary>
+        /// <param name="ssmlText">input ssml text</param>
+        /// <param name="intData">intermediate data</param>
+        /// <returns>processed ssml</returns>
         string Normalize(string ssmlText, out TInt intData);
     }
 }
